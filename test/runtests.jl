ea7be5df55343ec92a7ef858a79151f94228d41d
Kirill Simonov <xi@resolvent.net>
2018-11-22 20:11:01 -0600

    Added NarrativeTest as a test-specific dependency

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,5 @@
 #!/usr/bin/env julia
 
-using Pkg
-haskey(Pkg.installed(), "NarrativeTest") || Pkg.clone("https://github.com/rbt-lang/NarrativeTest.jl")
-
 using PPrint
 using NarrativeTest
 runtests()
