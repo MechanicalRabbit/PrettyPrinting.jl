a2ee4609034594dd07d8daaef8ecaba5aed6432e
Kyrylo Simonov <xi@resolvent.net>
2024-02-18 20:14:03 -0600

    Compatibility with the latest Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -4,13 +4,13 @@ using Documenter
 using PrettyPrinting
 
 # Highlight indented code blocks as Julia code.
-using Documenter.Expanders: ExpanderPipeline, Selectors, Markdown, iscode
-abstract type DefaultLanguage <: ExpanderPipeline end
+using Documenter: Expanders, Selectors, MarkdownAST, iscode
+abstract type DefaultLanguage <: Expanders.ExpanderPipeline end
 Selectors.order(::Type{DefaultLanguage}) = 99.0
 Selectors.matcher(::Type{DefaultLanguage}, node, page, doc) =
     iscode(node, "")
 Selectors.runner(::Type{DefaultLanguage}, node, page, doc) =
-    page.mapping[node] = Markdown.Code("julia", node.code)
+    node.element = MarkdownAST.CodeBlock("julia", node.element.code)
 
 makedocs(
     sitename = "PrettyPrinting.jl",
@@ -21,7 +21,10 @@ makedocs(
         "api.md",
         "test.md",
     ],
-    modules = [PrettyPrinting])
+    modules = [PrettyPrinting],
+    doctest = false,
+    repo = Remotes.GitHub("MechanicalRabbit", "PrettyPrinting.jl"),
+)
 
 deploydocs(
     repo = "github.com/MechanicalRabbit/PrettyPrinting.jl.git",
